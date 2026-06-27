96f8fe37c44f2437d189f2e1e9fc73639c9aa545
dhruvim.03 <dhruvimishra23@gmail.com>
2026-06-27 23:54:05 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,11 @@ const Order = require("./routes/order");
 app.use(express.json({ limit: "5mb" })); 
 app.use(
   cors({
-    origin: [
-      "https://sprightly-centaur-6e1ca3.netlify.app",
-      "http://localhost:5173"
-    ],
+   origin: [
+  "https://script-aura.vercel.app",
+  "https://sprightly-centaur-6e1ca3.netlify.app",
+  "http://localhost:5173"
+],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "id", "bookid"]
