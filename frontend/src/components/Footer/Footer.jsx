d7e0fd1ad599d584a402b07fe0cb00f5b4098489
dhruvim.03 <dhruvimishra23@gmail.com>
2026-06-28 01:24:00 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div className="bg-stone-800 text-amber-50 px-8 py-4">
-        <h1 className="text-2xl font-semibold text-center">
-            &copy; 2026, Made with love by DHRUVI MISHRA
+    <div className="bg-stone-800 text-amber-50 px-8 py-2">
+        <h1 className="text-sm font-medium text-center">
+            &copy; 2026 ScriptAura &mdash; Crafted with love by Dhruvi<3
         </h1>
     </div>
   );
